4cb4bc5ca55293a8bb59a488ffffbf1a559b0b1a
bertonoon <filipow.bartosz@gmail.com>
2023-02-06 00:35:01 +0100

    Headers

--- app/src/main/java/com/example/climbingtraining/model/BasicHangboardTimes.kt
+++ app/src/main/java/com/example/climbingtraining/model/BasicHangboardTimes.kt
@@ -3,9 +3,9 @@ package com.example.climbingtraining.model
 object BasicHangboardTimes {
     private val basicHangboardTimes = SimpleHangboard(
         prepareTime = 1000L,
-        hangTime = 1000L,
-        pauseTime = 1500L,
-        restTime = 1000L,
+        hangTime = 10000L,
+        pauseTime = 15000L,
+        restTime = 10000L,
         numberOfRounds = 2,
         numberOfSets = 2
     )
